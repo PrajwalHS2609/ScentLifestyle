7decb7b3ca62830fece95eff5557b6a67dd00936
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-05-14 22:42:37 +0530

    responsive Changes

--- src/Components/ContactPage/ContactPage.jsx
+++ src/Components/ContactPage/ContactPage.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import "./ContactPage.css"
-import LocationNavBg from './../LocationPage/LocationNavBg/LocationNavBg';
-import HelpNearYou from '../Help/HelpNearYou';
-import LastComp from '../LastComp/LastComp';
-import ContactMain from './ContactMain';
+import React from "react";
+import "./ContactPage.css";
+import LocationNavBg from "./../LocationPage/LocationNavBg/LocationNavBg";
+import HelpNearYou from "../Help/HelpNearYou";
+import LastComp from "../LastComp/LastComp";
+import ContactMain from "./ContactMain";
+import { Helmet } from "react-helmet";
 const ContactPage = () => {
   return (
-    <div className='contactPageContainer'>
-      <LocationNavBg/>
-      <ContactMain/>
-      <HelpNearYou/>
-      <LastComp/>
+    <div className="contactPageContainer">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Contact SCENT Salon for Services, Membership & Franchise</title>
+        <meta
+          name="Contact SCENT Salon for all your beauty needs, including services, membership benefits, and franchise opportunities. We're here to help you look and feel your best!"
+          content="Scent application"
+        />
+      </Helmet>
+      <LocationNavBg />
+      <ContactMain />
+      <HelpNearYou />
+      <LastComp />
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
