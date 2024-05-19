1e3c0c2333476608133aec1d40b8abe08a0d9d17
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-05-19 11:43:15 +0530

    changes

--- src/Components/AboutMyselfPage/AboutMyselfPage.jsx
+++ src/Components/AboutMyselfPage/AboutMyselfPage.jsx
@@ -2,21 +2,38 @@ import React from "react";
 import AboutMyselfTxt from "./AboutMyselfTxt";
 import AboutMyselfImg from "./AboutMyselfImg";
 import AboutMyselfContact from "./AboutMyselfContact";
-import "./AboutMyselfPage.css"
+import "./AboutMyselfPage.css";
 import AboutMyselfNavBg from "./AboutMyselfNavBg";
 import LastComp from "../LastComp/LastComp";
+import Members from "../HomePage/Members/Members";
+import OurSalon from "../HomePage/OurSalon/OurSalon";
+import Partners from "../HomePage/Partners/Partners";
+import InstaFeed from "../HomePage/InstaFeed/InstaFeed";
+import HelpNearYou from "../Help/HelpNearYou";
+import TextLocReviews from "../TextLocReviews/TextLocReviews";
+import ServiceWhy from "../MainServicePages/ServiceWhy/ServiceWhy";
 
 const AboutMyselfPage = () => {
   return (
-    <div className="aboutMyselfPageContainer">
-      <AboutMyselfNavBg/>
-      <AboutMyselfImg />
-      <AboutMyselfTxt />
-      <AboutMyselfContact />
-      
-      <LastComp/> 
-
-    </div>
+    <>
+      <div className="aboutMyselfPageContainer">
+        <AboutMyselfNavBg />
+        <AboutMyselfImg />
+        <AboutMyselfTxt />
+        <AboutMyselfContact />
+      </div>
+      <div>
+        {" "}
+        <Members />
+        <OurSalon />
+        <Partners />
+        <InstaFeed />
+        <HelpNearYou />
+        <TextLocReviews />
+        <ServiceWhy />
+        <LastComp />
+      </div>
+    </>
   );
 };
 
--- src/Components/StyleCrewPage/StyleCrewPage.jsx
+++ src/Components/StyleCrewPage/StyleCrewPage.jsx
@@ -5,6 +5,13 @@ import StyleCrewImg from "./StyleCrewImg";
 import "./StyleCrewPage.css";
 import LastComp from "../LastComp/LastComp";
 import NavBg from "../NavBg/NavBg";
+import OurSalon from "../HomePage/OurSalon/OurSalon";
+import Members from "../HomePage/Members/Members";
+import Partners from "../HomePage/Partners/Partners";
+import InstaFeed from "../HomePage/InstaFeed/InstaFeed";
+import HelpNearYou from "../Help/HelpNearYou";
+import TextLocReviews from "../TextLocReviews/TextLocReviews";
+import ServiceWhy from "../MainServicePages/ServiceWhy/ServiceWhy";
 
 const StyleCrewPage = () => {
   return (
@@ -13,6 +20,13 @@ const StyleCrewPage = () => {
       <StyleCrewHead />
       <StyleCrewPara />
       <StyleCrewImg />
+      <Members />
+      <OurSalon />
+      <Partners />
+      <InstaFeed />
+      <HelpNearYou />
+      <TextLocReviews />
+      <ServiceWhy />
       <LastComp />
     </div>
   );
