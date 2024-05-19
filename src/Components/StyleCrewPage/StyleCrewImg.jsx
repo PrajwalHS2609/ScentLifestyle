b03d5cb006a0312823bdc66e9ee9acc2d227a7cc
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-05-19 09:58:32 +0530

    img changes

--- src/Components/StyleCrewPage/StyleCrewImg.jsx
+++ src/Components/StyleCrewPage/StyleCrewImg.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import "./StyleCrewPage.css"
-import StyleCrewImages from './StyleCrewImages'
-
+import React from "react";
+import "./StyleCrewPage.css";
+import StyleCrewImages from "./StyleCrewImages";
+import crew1 from "./../../Images/Rose - Salon Director.jpeg.jpg";
+import crew2 from "./../../Images/Sam - Creative Director.jpg";
+import crew3 from "./../../Images/Lobsang - Top Stylist.jpg";
+import crew4 from "./../../Images/Kevin - Top Stylist.jpg";
 const StyleCrewImg = () => {
   return (
-    <div className='styleCrewImgContainer'>
+    <div className="styleCrewImgContainer">
       {/* <div className="styleCrewUnderline"></div> */}
       <div className="styleCrewImgContent">
-<StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/3.png"/>
-<StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/6.png"/>
-<StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/1.png"/>
-<StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/4.png "/>
-<StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/2.png"/>
-<StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/5.png"/>
-{/* <StyleCrewImages img="https://scentlifestyle.com/wp-content/uploads/2019/02/20p7-scent-JK-47.jpg"/>
-<StyleCrewImages img="https://scentlifestyle.com/wp-content/uploads/2019/02/20p7-scent-JK-49.jpg"/> */}
-
+        <StyleCrewImages img={crew1} />
+        <StyleCrewImages img={crew2} />
+        <StyleCrewImages img={crew3} />
+        <StyleCrewImages img={crew4} />
+        <StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/2.png" />
+        <StyleCrewImages img="https://website2.tryseoservices.com/wp-content/uploads/2024/03/5.png" />
       </div>
       {/* <div className="styleCrewUnderline"></div> */}
     </div>
-  )
-}
+  );
+};
 
-export default StyleCrewImg
+export default StyleCrewImg;
