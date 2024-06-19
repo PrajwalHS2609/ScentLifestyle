060931eda2c42fc3faba0307b909924fdb0adace
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-06-19 11:48:55 +0530

    changes

--- src/Components/ShopPage/Olaplex/OlaplexCard.jsx
+++ src/Components/ShopPage/Olaplex/OlaplexCard.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import "./Olaplex.css";
 
 const OlaplexCard = (props) => {
   return (
-    <div className='olaplexCardContainer'>
-       <div className="olaplexCardImg">
+    <div className="olaplexCardContainer">
+      <div className="olaplexCardImg">
         <img src={props.img} alt="" />
       </div>
       <div className="olaplexCardTxt">
         <div className="olaplexCardHead">
           <h2>{props.txt}</h2>
-  </div>
+        </div>
         <button>
-        <a href="https://api.whatsapp.com/send?phone=919742232700">
-          Buy 
-          <FontAwesomeIcon
-            icon={faWhatsapp}
-            className="olaplexWhats"
-          ></FontAwesomeIcon>
+          <a href="https://api.whatsapp.com/send?phone=919742232700">
+            Buy
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              className="olaplexWhats"
+            ></FontAwesomeIcon>
           </a>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OlaplexCard
+export default OlaplexCard;
