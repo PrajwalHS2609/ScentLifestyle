2e86d0cbe3e2e4579ac825cba561f05ed1faba30
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-07-08 14:09:32 +0530

    offer page

--- src/Components/OfferPage/OfferBenefits.jsx
+++ src/Components/OfferPage/OfferBenefits.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./OfferPage.css";
+import repair from "./../../Images/Repair.jpg";
+import shine from "./../../Images/Shine.jpg";
+import frizz from "./../../Images/Frizz.jpg";
+import protection from "./../../Images/Protection.jpg";
 
 const OfferBenefits = () => {
   return (
@@ -7,10 +11,7 @@ const OfferBenefits = () => {
       <div className="offer">
         <div className="offerImgContainer">
           <div className="offerImgContent">
-            <img
-              src="https://img.freepik.com/free-photo/high-angle-woman-using-dry-shampoo-home_23-2150706313.jpg?t=st=1720416178~exp=1720419778~hmac=7b4f68a2f710595a2f9a053acc1f920fbcf70b22392881fecd62f02497171f3e&w=996"
-              alt=""
-            />
+            <img src={repair} alt="" />
           </div>
         </div>
         <div className="offerHead">
@@ -20,10 +21,7 @@ const OfferBenefits = () => {
       <div className="offer">
         <div className="offerImgContainer">
           <div className="offerImgContent">
-            <img
-              src="https://img.freepik.com/free-photo/medium-shot-woman-giving-herself-scalp-massage_23-2151262283.jpg?w=996&t=st=1720426647~exp=1720427247~hmac=a4347f447ee0eff6a9bf7e27ac6e152f99f980f1780c804d5953922e1b69a589"
-              alt=""
-            />
+            <img src={frizz} alt="" />
           </div>
         </div>
         <div className="offerHead">
@@ -33,10 +31,7 @@ const OfferBenefits = () => {
       <div className="offer">
         <div className="offerImgContainer">
           <div className="offerImgContent">
-            <img
-              src="https://img.freepik.com/free-photo/beautiful-keratin-treated-hair_23-2149332492.jpg?t=st=1720417030~exp=1720420630~hmac=a0273379c5030f70560b3e9a159e1fb381016e3d08088ecfd2930d491559cbb6&w=996"
-              alt=""
-            />
+            <img src={shine} alt="" />
           </div>
         </div>
         <div className="offerHead">
@@ -46,10 +41,7 @@ const OfferBenefits = () => {
       <div className="offer">
         <div className="offerImgContainer">
           <div className="offerImgContent">
-            <img
-              src="https://img.freepik.com/free-photo/front-view-young-woman-with-prosthetis_23-2150589257.jpg?t=st=1720426834~exp=1720430434~hmac=5d7110b99d64915fc0c13d8074ef319644594c995803f76da44804023d69aea3&w=996"
-              alt=""
-            />
+            <img src={protection} alt="" />
           </div>
         </div>
         <div className="offerHead">
