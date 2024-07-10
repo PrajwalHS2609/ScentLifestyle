410d50c17bbf5040cc6d129bf5323830e985f850
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-07-10 12:47:19 +0530

    font changes

--- src/Components/HomePage/OurSalon/OurSalonFooterTxt.jsx
+++ src/Components/HomePage/OurSalon/OurSalonFooterTxt.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 const OurSalonFooterTxt = () => {
   return (
     <div className="salonContent3">
-    <p>OUR SALONS </p>
-    <FontAwesomeIcon
-      icon={faAngleRight}
-      className="salonIcon"
-    ></FontAwesomeIcon>
-  </div>
-  )
-}
+      <Link to="/">
+        <p>OUR SALONS </p>
+        <FontAwesomeIcon
+          icon={faAngleRight}
+          className="salonIcon"
+        ></FontAwesomeIcon>
+      </Link>
+    </div>
+  );
+};
 
-export default OurSalonFooterTxt
+export default OurSalonFooterTxt;
