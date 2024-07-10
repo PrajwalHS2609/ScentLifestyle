e84f5c5fe33fd9c096df7f35b78da3efae693f6d
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-07-10 10:00:55 +0530

    offer changes

--- src/Components/OfferPage/OfferTxt.jsx
+++ src/Components/OfferPage/OfferTxt.jsx
@@ -4,13 +4,13 @@ const OfferTxt = () => {
   return (
     <div className="offerTxtContainer">
       <div className="offerTxtContent" id="offerTxtContent1">
-        <h3>Book Now and</h3>
+        <h3>Book Now </h3>
       </div>
       <div className="offerTxtWrapper">
         <div className="offerTxtContent" id="offerTxtContent2">
-          <sup>Get</sup>
-          <h3>20%</h3>
-          <sub>Off</sub>
+          <sup>for</sup>
+          <h3>Rs.7999/-</h3>
+          <sub>Get Free HairCut</sub>
         </div>
       </div>
       <div className="offerTxtContent" id="offerTxtContent3">
