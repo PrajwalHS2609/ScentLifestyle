5b401db18944db130d1c395e75549baa8333fc88
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-08-03 14:50:27 +0530

    txt changes

--- src/Components/Lashes/LashesInnerPage/LashesExtension/LashesExtensionImg.jsx
+++ src/Components/Lashes/LashesInnerPage/LashesExtension/LashesExtensionImg.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LashesExtensionImg = () => {
   return (
     <div className="headSpaImgContainer">
-      <img src={""} alt="aromaSpaImg" />
+      <img src={""} alt="lashesExtensionImg" />
     </div>
   );
 };
