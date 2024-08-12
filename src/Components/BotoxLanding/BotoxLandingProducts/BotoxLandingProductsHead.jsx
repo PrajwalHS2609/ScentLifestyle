0b0ce875dabce846acc51477d0df0ff3fc932f86
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-08-12 17:15:28 +0530

    product para

--- src/Components/BotoxLanding/BotoxLandingProducts/BotoxLandingProductsHead.jsx
+++ src/Components/BotoxLanding/BotoxLandingProducts/BotoxLandingProductsHead.jsx
@@ -8,15 +8,13 @@ const BotoxLandingProductsHead = () => {
       <div className="botoxProductsHeadContent">
         <img src={beautyGarage} alt="beautyGarage" />
         <h3>Botoplexx</h3>
-        <p>For Dry, Brittle & Color treated hair</p>
-        <p className="paraTxt">
-          <p>
-            Paint your Hair! <br />
-            Paint it Red & Blue, with
-            <br />
-          </p>
-          <span> ZERO WORRIES! </span>
+        <p className="paraTxt1">For Dry, Brittle & Color treated hair</p>
+        <p className="paraTxt2">
+          Paint your Hair! <br />
+          Paint it Red & Blue, with
+          <br />
         </p>
+        <p className="paraTxt3"> ZERO WORRIES!</p>
         <a href="https://api.whatsapp.com/send?phone=919742232700">
           <button>Book in Appointment</button>
         </a>
