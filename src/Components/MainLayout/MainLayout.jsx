6542145311b01384c67d91f479a99241cb98f635
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-08-12 14:44:53 +0530

    nav bar removal

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -5,10 +5,10 @@ import { useLocation } from "react-router-dom";
 const MainLayout = ({ children }) => {
   const location = useLocation();
   const hideNavbar1 = location.pathname !== "/offer";
-  const hideNavbar2 = location.pathname !== "/botox-landing";
-  const hideNavbar3 = location.pathname !== "/hair-treatment-landing";
-  const hideNavbar4 = location.pathname !== "/olaplex-landing";
-  const hideNavbar5 = location.pathname !== "/hair-color-streak-landing";
+  const hideNavbar2 = location.pathname !== "/lp-hair-botoxx";
+  const hideNavbar3 = location.pathname !== "/lp-hair-treatment";
+  const hideNavbar4 = location.pathname !== "/lp-olaplex";
+  const hideNavbar5 = location.pathname !== "/lp-hair-highlights-and-streaks";
 
   return (
     <div>
