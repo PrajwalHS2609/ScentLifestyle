79e9b86a7bee9da695f06e2d2a621ef51c9099fd
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-08-21 13:45:33 +0530

    mobile and desk

--- src/Components/OfferTesting/OfferTestingImg/OfferTestingImg.jsx
+++ src/Components/OfferTesting/OfferTestingImg/OfferTestingImg.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./OfferTestingImg.css";
-import happyHoursImg from "./../../../Images/OfferImgs/happyHoursImg.png";
-import nailExtensionImg from "./../../../Images/OfferImgs/nailsExtensionsImg.png";
-import waxingImg from "./../../../Images/OfferImgs/waxingImg.png";
+// import happyHoursImg from "./../../../Images/OfferImgs/happyHoursImg.png";
+// import nailExtensionImg from "./../../../Images/OfferImgs/nailsExtensionsImg.png";
+// import waxingImg from "./../../../Images/OfferImgs/waxingImg.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import happyHoursImg from "./../../../Images/OfferImgs/happyHoursImg.png";
+import happyHoursImgMobile from "./../../../Images/OfferImgs/happyHoursImgMobile.png";
+import nailsExtensionsImg from "./../../../Images/OfferImgs/nailsExtensionsImg.png";
+import nailsExtensionsImgMobile from "./../../../Images/OfferImgs/nailsExtensionsImgMobile.png";
+import waxingImg from "./../../../Images/OfferImgs/waxingImg.png";
+import waxingImgMobile from "./../../../Images/OfferImgs/waxingImgMobile.png";
 // import leftArrow from "./../../../Images/left.png";
 // import rightArrow from "./../../../Images/right.png";
 // import { Link } from "react-router-dom";
@@ -25,19 +31,34 @@ const OfferTestingImg = () => {
     <div className="OfferTestingImg">
       <Slider {...settings} className="offerTestingSlider">
         <div className="OfferTestingImgContent">
-          <img src={happyHoursImg} alt="happyHoursImg" />
+          <img className="deskImg" src={happyHoursImg} alt="happyHoursImg" />
+          <img
+            className="mobileImg"
+            src={happyHoursImgMobile}
+            alt="happyHoursImgMobile"
+          />
           <a href="https://api.whatsapp.com/send?phone=919606682700">
             <button>BOOK NOW</button>
           </a>
         </div>
         <div className="OfferTestingImgContent">
-          <img src={nailExtensionImg} alt="nailExtensionImg" />
+          <img className="deskImg" src={nailsExtensionsImg} alt="nailsExtensionsImg" />
+          <img
+            className="mobileImg"
+            src={nailsExtensionsImgMobile}
+            alt="nailsExtensionsImgMobile"
+          />{" "}
           <a href="https://api.whatsapp.com/send?phone=919606682700">
             <button>BOOK NOW</button>
           </a>
         </div>
         <div className="OfferTestingImgContent">
-          <img src={waxingImg} alt="waxingImg" />
+          <img className="deskImg" src={waxingImg} alt="waxingImg" />
+          <img
+            className="mobileImg"
+            src={waxingImgMobile}
+            alt="waxingImgMobile"
+          />{" "}
           <a href="https://api.whatsapp.com/send?phone=919606682700">
             <button>BOOK NOW</button>
           </a>
