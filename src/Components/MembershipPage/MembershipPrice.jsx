fd4d8744dd8a7fd4a07687fffb1de7fd2ad506bb
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-09-01 12:30:09 +0530

    membership

--- src/Components/MembershipPage/MembershipPrice.jsx
+++ src/Components/MembershipPage/MembershipPrice.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MemberShipPriceCard from "./MemberShipPriceCard";
+// import MemberShipPriceCard from "./MemberShipPriceCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faIndianRupee } from "@fortawesome/free-solid-svg-icons";
 const MembershipPrice = () => {
@@ -21,109 +21,28 @@ const MembershipPrice = () => {
         <div className="membershipPriceItem5">
           <h3>VALIDITY</h3>
         </div>
-        <div className="membershipPriceItem6">
-          <h3>SILVER</h3>
+        <div className="membershipPriceItem31">
+          <h3>PLATINUM</h3>
         </div>
-        <div className="membershipPriceItem7">
+        <div className="membershipPriceItem32">
           <h3>
             <FontAwesomeIcon
               icon={faIndianRupee}
               className="menuIcon"
             ></FontAwesomeIcon>{" "}
-            10000
+            100000
           </h3>
         </div>
-        <div className="membershipPriceItem8">
+        <div className="membershipPriceItem33">
           <h3>
             <FontAwesomeIcon
               icon={faIndianRupee}
               className="menuIcon"
             ></FontAwesomeIcon>{" "}
-            11000
+            150000
           </h3>
         </div>
-        <div className="membershipPriceItem9">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            1000
-          </h3>
-        </div>
-        <div className="membershipPriceItem10">
-          <h3>1 YEAR</h3>
-        </div>
-        <div className="membershipPriceItem11">
-          <h3>RUBY</h3>
-        </div>
-        <div className="membershipPriceItem12">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            20000
-          </h3>
-        </div>
-        <div className="membershipPriceItem13">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            27000
-          </h3>
-        </div>
-        <div className="membershipPriceItem14">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            7000
-          </h3>
-        </div>
-        <div className="membershipPriceItem15">
-          <h3>1 YEAR</h3>
-        </div>
-        <div className="membershipPriceItem16">
-          <h3>GOLD</h3>
-        </div>
-        <div className="membershipPriceItem17">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            30000
-          </h3>
-        </div>
-        <div className="membershipPriceItem18">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            45000
-          </h3>
-        </div>
-        <div className="membershipPriceItem19">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            15000
-          </h3>
-        </div>
-        <div className="membershipPriceItem20">
-          <h3>1 YEAR</h3>
-        </div>
-        <div className="membershipPriceItem21">
-          <h3>DIAMOND</h3>
-        </div>
-        <div className="membershipPriceItem22">
+        <div className="membershipPriceItem34">
           <h3>
             <FontAwesomeIcon
               icon={faIndianRupee}
@@ -132,26 +51,8 @@ const MembershipPrice = () => {
             50000
           </h3>
         </div>
-        <div className="membershipPriceItem23">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            75000
-          </h3>
-        </div>
-        <div className="membershipPriceItem24">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            25000
-          </h3>
-        </div>
-        <div className="membershipPriceItem25">
-          <h3>2 YEAR</h3>
+        <div className="membershipPriceItem35">
+          <h3>3 YEARS</h3>
         </div>
         <div className="membershipPriceItem26">
           <h3>SAPPHIRE</h3>
@@ -186,28 +87,10 @@ const MembershipPrice = () => {
         <div className="membershipPriceItem30">
           <h3>2 YEARS</h3>
         </div>
-        <div className="membershipPriceItem31">
-          <h3>PLATINUM</h3>
+        <div className="membershipPriceItem21">
+          <h3>DIAMOND</h3>
         </div>
-        <div className="membershipPriceItem32">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            100000
-          </h3>
-        </div>
-        <div className="membershipPriceItem33">
-          <h3>
-            <FontAwesomeIcon
-              icon={faIndianRupee}
-              className="menuIcon"
-            ></FontAwesomeIcon>{" "}
-            150000
-          </h3>
-        </div>
-        <div className="membershipPriceItem34">
+        <div className="membershipPriceItem22">
           <h3>
             <FontAwesomeIcon
               icon={faIndianRupee}
@@ -216,8 +99,127 @@ const MembershipPrice = () => {
             50000
           </h3>
         </div>
-        <div className="membershipPriceItem35">
-          <h3>3 YEARS</h3>
+        <div className="membershipPriceItem23">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            75000
+          </h3>
+        </div>
+        <div className="membershipPriceItem24">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            25000
+          </h3>
+        </div>
+        <div className="membershipPriceItem25">
+          <h3>2 YEAR</h3>
+        </div>
+        <div className="membershipPriceItem16">
+          <h3>GOLD</h3>
+        </div>
+        <div className="membershipPriceItem17">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            30000
+          </h3>
+        </div>
+        <div className="membershipPriceItem18">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            45000
+          </h3>
+        </div>
+        <div className="membershipPriceItem19">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            15000
+          </h3>
+        </div>
+        <div className="membershipPriceItem20">
+          <h3>1 YEAR</h3>
+        </div>
+
+        <div className="membershipPriceItem11">
+          <h3>RUBY</h3>
+        </div>
+        <div className="membershipPriceItem12">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            20000
+          </h3>
+        </div>
+        <div className="membershipPriceItem13">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            27000
+          </h3>
+        </div>
+        <div className="membershipPriceItem14">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            7000
+          </h3>
+        </div>
+        <div className="membershipPriceItem15">
+          <h3>1 YEAR</h3>
+        </div>
+
+        <div className="membershipPriceItem6">
+          <h3>SILVER</h3>
+        </div>
+        <div className="membershipPriceItem7">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            10000
+          </h3>
+        </div>
+        <div className="membershipPriceItem8">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            11000
+          </h3>
+        </div>
+        <div className="membershipPriceItem9">
+          <h3>
+            <FontAwesomeIcon
+              icon={faIndianRupee}
+              className="menuIcon"
+            ></FontAwesomeIcon>{" "}
+            1000
+          </h3>
+        </div>
+        <div className="membershipPriceItem10">
+          <h3>1 YEAR</h3>
         </div>
       </div>
     </div>
