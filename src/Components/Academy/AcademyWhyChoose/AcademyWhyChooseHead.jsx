8b4ba5d7ffbaaa003a719883a6310797a4cef657
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-12-09 20:12:40 +0530

    academy resp

--- src/Components/Academy/AcademyWhyChoose/AcademyWhyChooseHead.jsx
+++ src/Components/Academy/AcademyWhyChoose/AcademyWhyChooseHead.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AcademyWhyChooseHead = () => {
   return (
     <div className="academyWhyChoose-headContainer">
-      <h2>Why Choose SentLifestyle</h2>
+      <h2>Why Choose SCENTLifestyle</h2>
     </div>
   )
 }
