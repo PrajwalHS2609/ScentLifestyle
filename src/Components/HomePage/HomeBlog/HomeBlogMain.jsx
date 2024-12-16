9e7e28c573016763b8e0a767228d641e9c1ed418
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-12-16 18:33:30 +0530

    xml sitemap

--- src/Components/HomePage/HomeBlog/HomeBlogMain.jsx
+++ src/Components/HomePage/HomeBlog/HomeBlogMain.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import "./HomeBlog.css";
 import HomeBlogCard from "./HomeBlogCard";
-import hairBleachBanner from "./../../../Images/Blog/How Often Should You Bleach Your Hair/Banner.png";
+import academyEnrollBanner from "./../../../Images/Blog/Top_10_Reasons_to_ Enroll_in_ Salon_Academy/Banner.png";
 import bodyWaxingMythBanner from "./../../../Images/Blog/Body_waxing_myth/Banner.png";
 import christmasOfferBanner from "./../../../Images/Blog/Christmas_offer/Banner.png";
 import { Link } from "react-router-dom";
+import BlogCard from "../../BlogPage/BlogPages/BlogCard";
 const HomeBlogMain = () => {
   return (
     <div className="homeBlogMain">
       <div className="homeBlogMainContainer">
-        <HomeBlogCard
-          img={hairBleachBanner}
-          head="How Often Should You Bleach Your Hair? Expert Advice"
-          para="Bleaching your hair can be a transformative experience, offering a fresh and..."
-          link="/how-often-should-you-bleach-your-hair"
-        />
         <HomeBlogCard
           img={bodyWaxingMythBanner}
           head="The Truth About Body Waxing: Myths, Facts, and Tips"
@@ -26,6 +21,12 @@ const HomeBlogMain = () => {
           head="Get Festive Ready: Top Christmas Beauty Tips for Women & Men"
           para="The holiday season is here, and with it comes an exciting opportunity to get festive, enjoy the celebrations, and..."
           link="/get-festive-ready-top-christmas-beauty-tips-for-women-and-men"
+        />
+        <HomeBlogCard
+          img={academyEnrollBanner}
+          head="Top 10 Reasons to Enroll in Our Premier Salon Academy Today"
+          para="Are you passionate about beauty, hair, or makeup and dream of turning your passion into a successful career?..."
+          link="/top-10-reasons-to-enroll-in-our-premier-salon-academy-today"
         />
       </div>
       <div className="homeBlogBtn">
