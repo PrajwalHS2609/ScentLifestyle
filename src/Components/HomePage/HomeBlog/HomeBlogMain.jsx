3981b12c94369eeec6c4350ddfe2b9df6af76864
Prajwal H S <prajwalhs4india@gmail.com>
2025-01-16 19:20:13 +0530

    home blog

--- src/Components/HomePage/HomeBlog/HomeBlogMain.jsx
+++ src/Components/HomePage/HomeBlog/HomeBlogMain.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import "./HomeBlog.css";
 import HomeBlogCard from "./HomeBlogCard";
-import academyEnrollBanner from "./../../../Images/Blog/Top_10_Reasons_to_ Enroll_in_ Salon_Academy/Banner.png";
+import clearerBanner from "./../../../Images/Blog/Clearer Skin, Happier You Acne Cure Facial Secrets/Banner.png";
 import sheaButterBanner from "./../../../Images/Blog/Shea Butter Hair Spa/Banner.png";
-import christmasOfferBanner from "./../../../Images/Blog/Christmas_offer/Banner.png";
+import pedicureTreatmentDryBanner from "./../../../Images/Blog/Best Pedicure Treatments for Dry and Cracked Heels/Banner.png";
 import { Link } from "react-router-dom";
 const HomeBlogMain = () => {
   return (
     <div className="homeBlogMain">
       <div className="homeBlogMainContainer">
         <HomeBlogCard
+          img={pedicureTreatmentDryBanner}
+          head="Best Pedicure Treatments for Dry and Cracked Heels"
+          para="Dry and cracked heels can be a common yet frustrating issue, especially for those who are on their feet all day or..."
+          link="/best-pedicure-treatments-for-dry-and-cracked-heel"
+        />{" "}
+        <HomeBlogCard
           img={sheaButterBanner}
           head="Revitalize Your Hair with the Magic of Shea Butter Hair Spa!"
           para="Hair is one of the most defining aspects of our appearance, yet it often suffers from exposure to pollution..."
           link="/revitalize-you-hair-with-the-magic-of-shea-butter-hair-spa"
-        />{" "}
-        <HomeBlogCard
-          img={academyEnrollBanner}
-          head="Top 10 Reasons to Enroll in Our Premier Salon Academy Today"
-          para="Are you passionate about beauty, hair, or makeup and dream of turning your passion into a successful career?..."
-          link="/top-10-reasons-to-enroll-in-our-premier-salon-academy-today"
         />
         <HomeBlogCard
-          img={christmasOfferBanner}
-          head="Get Festive Ready: Top Christmas Beauty Tips for Women & Men"
-          para="The holiday season is here, and with it comes an exciting opportunity to get festive, enjoy the celebrations, and..."
-          link="/get-festive-ready-top-christmas-beauty-tips-for-women-and-men"
+          img={clearerBanner}
+          head="Clearer Skin, Happier You: Acne Cure Facial Secrets"
+          para="Acne is one of the most common skin concerns affecting people of all ages Whether it's a persistent issue or..."
+          link="/clearer-skin-happier-you-acne-cure-facial-secrets"
         />
       </div>
       <div className="homeBlogBtn">
