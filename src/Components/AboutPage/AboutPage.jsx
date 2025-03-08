6aa965eb6098531a2241ae9512f7ae7cf489d358
Prajwal H S <prajwalhs4india@gmail.com>
2025-03-08 17:31:31 +0530

    about helmet

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -4,6 +4,7 @@ import AboutHeader from "./AboutHeader";
 import AboutOwner from "./AboutOwner";
 import AboutContent from "./AboutContent";
 import AboutVision from "./AboutVision";
+import { Helmet } from "react-helmet";
 // import AboutEconomics from "./AboutEconomics";
 // import AboutMarket from "./AboutMarket";
 // import AboutSize from "./AboutSize";
@@ -11,10 +12,19 @@ import AboutVision from "./AboutVision";
 const AboutPage = () => {
   return (
     <div className="about-container">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Krishma Lobo - Owner of the SCENT Lifestyle</title>
+        <meta
+          name="description"
+          content="Scent is a sanctum where life comes to beauty and beauty to life. A makeover at scent will be the crown of your looks."
+        />
+        <link rel="canonical" href="https://scentlifestyle.com/about-us" />
+      </Helmet>
       <AboutHeader />
-      <AboutOwner/>
-      <AboutContent/>
-      <AboutVision/>
+      <AboutOwner />
+      <AboutContent />
+      <AboutVision />
       {/* <AboutEconomics/>
       <AboutMarket/>
       <AboutSize/>
